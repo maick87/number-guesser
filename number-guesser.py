5ca9e90985ff92eaf4fafa0ca5a1dcffa5ec28bc
maick87 <143333261+maick87@users.noreply.github.com>
2024-05-10 20:46:50 -0300

    Update number-guesser.py

--- number-guesser.py
+++ number-guesser.py
@@ -13,28 +13,28 @@ max_tries = 5
 while True or player_tries < max_tries:
 
     # Getting player's input
-    player_number = input('Digite um número: ' if player_tries < max_tries - 1 else 'Última tentativa!' )
+    player_number = input('Type a number: ' if player_tries < max_tries - 1 else 'Última tentativa!' )
 
     # Checking if the input is a valid integer
     try:
         player_number = int(player_number)
     except ValueError:
         # Informing the player to input only integers if the input is not a valid integer
-        print('Por favor, digite apenas números inteiros.')
+        print('Error. Type only integrers')
         continue  # Skipping the rest of the loop and asking for input again
     
     # Checking if player's guess is correct
     if player_number == numb_gen:
-        print('Parabéns! Você acertou!!')
+        print('You won!!')
         # Asking the player if they want to play again
-        player_input = input('Jogar novamente SIM/NAO = ').upper()
-        if player_input == 'SIM':
+        player_input = input('Play again YES/NO = ').upper()
+        if player_input == 'YES':
             # Resetting the game if the player wants to play again
             os.system('cls')  # Clearing the screen
             player_tries = 0
             numb_gen = random.randint(1, 100)
             continue
-        elif player_input == 'NAO':
+        elif player_input == 'NO':
             # Exiting the game if the player doesn't want to play again
             os.system('cls')
             print('thanks for using my code')
@@ -43,28 +43,28 @@ while True or player_tries < max_tries:
     # Informing the player to guess a higher number if their guess is lower than the generated number
     elif player_number < numb_gen:
         if player_tries < max_tries - 1:
-            print('Tente um número mais alto.')
+            print('Try a high number.')
         player_tries += 1
     
     # Informing the player to guess a lower number if their guess is higher than the generated number
     else:
         if player_tries < max_tries - 1:
-            print('Tente um número mais baixo.')
+            print'Try a low number.')
         player_tries += 1 
 
     # Checking if the player has used all their attempts
     if player_tries == max_tries:
         os.system('cls')  # Clearing the screen
-        print('Você perdeu!! O número correto era', numb_gen)
+        print('You lose!! The right number is = ', numb_gen)
         # Asking the player if they want to play again
-        player_input2 = input('Jogar novamente SIM/NAO = ').upper()
-        if player_input2 == 'SIM':
+        player_input2 = input('Play again YES/NO = ').upper()
+        if player_input2 == 'YES':
             # Resetting the game if the player wants to play again
             os.system('cls')
             player_tries = 0
             numb_gen = random.randint(1, 100)
             continue
-        elif player_input2 == 'NAO':
+        elif player_input2 == 'NO':
             # Exiting the game if the player doesn't want to play again
             os.system('cls')
             print('thanks for using my code')
